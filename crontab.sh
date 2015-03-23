ceb3c2aa0e1d38ac140508b42fdbca0fe8ed8037
Andre <andre.github@gmail.com>
2015-03-23 15:35:12 +0800

    Update

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH="/bin:/usr/bin:/usr/local/bin"
-echo 'crontab begin with: $cd /home/pi/home-server-v2 && $shjs /home/pi/home-server-v2/auto-update.js' >> /home/pi/home-server-v2/log/crontab.log &&
-cd /home/pi/home-server-v2 && shjs /home/pi/home-server-v2/auto-update.js >> /home/pi/home-server-v2/log/crontab.log &&
-echo 'crontab end' >> /home/pi/home-server-v2/log/crontab.log
+#export PATH="/bin:/usr/bin:/usr/local/bin"
+#echo 'crontab begin with: $cd /home/pi/home-server-v2 && $shjs /home/pi/home-server-v2/auto-update.js' >> /home/pi/home-server-v2/log/crontab.log &&
+#cd /home/pi/home-server-v2 && shjs /home/pi/home-server-v2/auto-update.js >> /home/pi/home-server-v2/log/crontab.log &&
+#echo 'crontab end' >> /home/pi/home-server-v2/log/crontab.log
 
